07591d90104c8f4c2282627acb14a046e4a197a9
James Frank <jfrankdev90@gmail.com>
2016-12-11 09:18:06 -0800

    Added favicon

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title></title>
-    <title></title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+    <title>Intuit Mint</title>
+
 
 
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
