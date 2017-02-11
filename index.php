40ee6b715720ebdc287246e11f3b1fe9fcbc52e2
James Frank <jfrankdev90@gmail.com>
2017-02-10 16:37:14 -0800

    Refactoring

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 <!DOCTYPE html>
-<html >
+<html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-
-
     <title>Intuit Mint</title>
-
-
-
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
 <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
-
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/animate.css">
-
-
-
   </head>
 <h1 id= "toptext" class="followers bounce animated"> See your complete financial picture. </h1>
   <body>
-
 <header id="banner" class="navbar navbar-default navbar-fixed-top">
   <div class="container"><a href="#top" class="scrollable"><img src="images/intuit_mint_logo_detail.png" alt=/></a>
     <button type="button" data-toggle="collapse" data-target="#navbar" aria-expanded="false" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
@@ -37,9 +27,7 @@
 </header>
         <div id="portfolioscroll"></div>
         <div id="tweetscroll"></div>
-
 <!-- end header-->
-
 <main>
   <header>
 <img src="images/mintlogo1.png" alt=""/>
@@ -47,7 +35,6 @@
   <!-- Gallery Grid Section-->
   <section id="portfolio" class="baloo aligncen">
     <h2>Your financial life, in one place and easy to understand.</h2>
-
     <ul>
       <p class= "col-sm-4 pull-down"><b>Custom tips and savings</b><br>Because Mint knows what’s going on with your money, we can help you find savings along the way. We analyze thousands of checking, savings, credit card, brokerage, CD and IRA rollover offers—then make recommendations that will save you the most based on your lifestyle and goals.</p>
       <img src="images/custom-tips-and-advice.jpg" alt="" height="303" width="479"/>
@@ -58,10 +45,8 @@
       <p class= "col-sm-4 pull3"><b>Simple and free to set up</b><br>It’s free and easy to get started, and we connect to almost every US financial institution connected to the internet. In just minutes, you’ll see where your money is going and get ideas on how to stretch it farther.</p>
       <img src="images/your-financial-life-in-one-place.png" alt="" height="303" width="479"/>
     </ul>
-
   </section>
   <!-- Twitter Section-->
-
   		<div id="tweets" class="boxed">
 <div id="lefttweet">
 <blockquote class="twitter-tweet tw-align-left" data-conversation="none" data-lang="en"><p lang="en" dir="ltr"><a href="https://twitter.com/mint">@mint</a> nevermind, your excellent support team helped me out!</p>&mdash; Sudil M. (@Sudsington) <a href="https://twitter.com/Sudsington/status/771042246451982336">August 31, 2016</a></blockquote>
@@ -73,19 +58,16 @@
 <blockquote class="twitter-tweet tw-align-center" data-width="300" data-lang="en"><p lang="en" dir="ltr">Do I have any followers who use <a href="https://twitter.com/mint">@mint</a> for tracking spending habits and have at least a few years of data? Preferably OS X users.</p>&mdash; Josh Clayton (@joshuaclayton) <a href="https://twitter.com/joshuaclayton/status/770933447053410304">August 31, 2016</a></blockquote>
 </div>
 </div>
-
 	<div class="followers"><span style="font-size:90px;"><?php include 'littleApp.php'; echo "$num";?></span><br>
-  </div><div class="followers2"> people follow us on twitter. 
+  </div><div class="followers2"> people follow us on twitter.
   <a href="https://twitter.com/mint" style="color:white;text-decoration:underline;" target="_blank">Join them!
   </a></p></div>
-
   <!-- Scroll to Top Button (Only supposed to be visible on small and small screen sizes)-->
   <div class="scroll-top"><a href="#top" class="button scrollable"><i class="fa fa-chevron-up"></i></a></div>
 </main>
 <!-- Footer-->
 <footer>
   <div id="footer-above" class="baloo">
-
     <div>
       <h3>Intuit® Around the Web</h3>
       <ul>
@@ -100,15 +82,10 @@
     <script>document.write(new Date().getFullYear())</script>. All Rights Reserved.
   </div>
 </footer>
-<!-- <aside id="html5badge"><img src="http://www.w3.org/html/logo/badge/html5-badge-v-css3-graphics-semantics.png" width="38" height="170" alt="HTML5 Powered with CSS3 / Styling, Graphics, 3D &amp; Effects, and Semantics" title="HTML5 Powered with CSS3 / Styling, Graphics, 3D &amp; Effects, and Semantics"></aside>-->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
 <script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
-
         <script src="js/index.js"></script>
 <script async src="js/widgets.js" charset="utf-8"></script>
-
-
-
   </body>
 </html>
